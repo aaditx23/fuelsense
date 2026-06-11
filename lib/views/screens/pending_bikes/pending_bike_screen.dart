a9ca6831aa6f954b28f4cc9c93a74f4488ee8aee
aaditx23 <adit.pranto@gmail.com>
2026-06-11 14:52:07 +0600

    Feat: Pending bikes search filter initial

--- lib/views/screens/pending_bikes/pending_bike_screen.dart
+++ lib/views/screens/pending_bikes/pending_bike_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fuelsense/data/remote/bike/schema/bike_model.dart';
 import 'package:fuelsense/views/screens/pending_bikes/pending_bikes_notifier.dart';
 import 'package:fuelsense/views/screens/pending_bikes/widgets/pending_bike_card.dart';
 import 'package:fuelsense/views/widgets/common_scaffold.dart';
@@ -25,13 +26,38 @@ class _PendingBikeScreenState extends ConsumerState<PendingBikeScreen> {
     );
   }
 
+  List<BikeModel> _filterBikes(List<BikeModel> bikes) {
+    if (_searchQuery.isEmpty) return bikes;
+    final query = _searchQuery.toLowerCase();
+    return bikes.where((bike) {
+      return bike.brand.toLowerCase().contains(query) ||
+          bike.model.toLowerCase().contains(query) ||
+          bike.modelYear.toString().contains(query) ||
+          bike.engineCc.toString().contains(query) ||
+          bike.fuelType.toLowerCase().contains(query) ||
+          bike.expectedMileage.toString().contains(query) ||
+          bike.tankCapacity.toString().contains(query) ||
+          (bike.reserveCapacity?.toString().contains(query) ?? false) ||
+          (bike.image?.toLowerCase().contains(query) ?? false) ||
+          bike.updatedAt.toLowerCase().contains(query);
+    }).toList();
+  }
+
   @override
   Widget build(BuildContext context) {
     final state = ref.watch(pendingBikesNotifierProvider);
+    final _filteredBikes = _filterBikes(state.pendingBikes);
     return CommonScaffold(
       body: Column(
         children: [
-          SearchBarWidget(onChanged: (value) {}, controller: _searchController),
+          SearchBarWidget(
+            onChanged: (value) {
+              setState(() {
+                _searchQuery = value;
+              });
+            },
+            controller: _searchController,
+          ),
           Expanded(
             child: state.isLoading
                 ? const Center(child: CircularProgressIndicator())
@@ -44,9 +70,9 @@ class _PendingBikeScreenState extends ConsumerState<PendingBikeScreen> {
                   )
                 : state.pendingBikes.isNotEmpty
                 ? ListView.builder(
-                    itemCount: state.pendingBikes.length,
+                    itemCount: _filteredBikes.length,
                     itemBuilder: (context, index) {
-                      final bike = state.pendingBikes[index];
+                      final bike = _filteredBikes[index];
                       return PendingBikeCard(bike: bike, onAdd: () {});
                     },
                   )
